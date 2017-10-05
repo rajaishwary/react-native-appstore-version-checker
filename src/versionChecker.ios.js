0fcf094744640a6c6209049232f2e2d25075b0cf
rajaishwary <rajaishwary@gmail.com>
2017-10-05 21:52:10 +0530

    ios mods for breaking changes

--- src/versionChecker.ios.js
+++ src/versionChecker.ios.js
@@ -1,8 +1,8 @@
 import result from 'lodash/result';
 import {get, parseJson} from './fetcher';
 
-const getAppstoreAppVersion = (identifier, { typeOfId = 'id', country = 'us' }) => {
-  const url = `http://itunes.apple.com/lookup?${typeOfId}=${identifier}&country=${country}`;
+const getAppstoreAppVersion = (identifier) => {
+  const url = `http://itunes.apple.com/lookup?id=${identifier}&country=in`;
   return get(url).then(parseJson).then((d) => {
     const version = result(d, 'data.results[0].version');
     if (!version) {
